65ab96a7cceb62660c9951ec1e40646502a07410
Jasmine Li <jxl9@cornell.edu>
2024-09-30 06:48:07 -0400

    research, writing sections

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,7 +2,115 @@ export default function Projects() {
   return (
     <div className="py-8">
       {/* <h1 className="text-3xl font-bold mb-4">My Projects</h1> */}
-      <p>Coming soon!</p>
+      <h2 className="text-2xl font-bold mt-6 mb-4">Research</h2>
+      <p>
+        I'm interested in AI alignment, analytical chemistry, and US-CN policy!{" "}
+      </p>
+      <ul className="pl-4">
+        <li className="mb-2">
+          <a
+            href="https://arxiv.org/pdf/2406.20087"
+            className="text-custom-green"
+          >
+            ProgressGym: Alignment with a Millennium of Moral Progress
+          </a>
+          <p className="italic">
+            NeurIPS 2024. Qiu, T., Zhang, Y., Huang, Z., Li, J.X., et al.{" "}
+          </p>
+        </li>
+        <li className="mb-2">
+          <a
+            href="https://drive.google.com/file/d/1-uK8lsKApXFikfCdQIixFNwu0uWL1gSJ/view"
+            className="text-custom-green"
+          >
+            Scaling laws for contrastive activation addition with refusal
+            mechanisms and Llama 2 models
+          </a>
+          <p className="italic">
+            SP24 Berkeley Supervised Program for Alignment Research, Mentor:
+            Abdur Raheem Ali
+          </p>
+        </li>
+        <li className="mb-2">
+          <a
+            href="https://www.nature.com/articles/s41467-023-39574-3"
+            className="text-custom-green"
+          >
+            Machining water through laser cutting of nanoparticle-encased water
+            pancakes
+          </a>
+          <p className="italic">June 2023, Nature Communications</p>
+        </li>
+
+        <li className="mb-2">
+          <a
+            href="https://pubs.acs.org/doi/full/10.1021/acs.analchem.0c04883"
+            className="text-custom-green"
+          >
+            A three-dimensional paper-based isoelectric focusing device for
+            direct analysis of proteins in physiological samples
+          </a>
+          <p className="italic">Feb. 2021, Analytical Chemistry</p>
+        </li>
+        <li className="mb-2">
+          <a
+            href="https://docs.google.com/document/d/10l-bHw6ldT6bkbe16vVth880QCvIHAemPjJxxxlihl4/edit?usp=sharing"
+            className="text-custom-green"
+          >
+            Proposal for cheaply evaluating dangerous biochemical experimental
+            capabilities in frontier large-language models
+          </a>
+          <p className="italic">Dec 5, 2023. Personal Research.</p>
+        </li>
+        <li className="mb-2">
+          <a href="" className="text-custom-green">
+            China's International Student Recruitment Strategy as a Failed
+            Exercise of Soft Power
+          </a>
+          <p className="italic">
+            Jan 6, 2022. Stanford Freeman Spogli Institute for International
+            Studies.
+          </p>
+        </li>
+      </ul>
+
+      {/* New Code Section */}
+      {/* <h2 className="text-2xl font-bold mt-6 mb-4">Code</h2>
+      <div className="grid grid-cols-3 gap-4">
+        <div className="border p-4">
+          <img
+            src="path/to/image1.jpg"
+            alt="Project 1"
+            className="w-full h-auto"
+          />
+          <a href="https://link-to-project1.com" className="text-custom-green">
+            Ritu
+          </a>
+          <p className="italic">Metadata for Project 1</p>
+        </div>
+        <div className="border p-4">
+          <img
+            src="path/to/image2.jpg"
+            alt="Project 2"
+            className="w-full h-auto"
+          />
+          <a href="https://link-to-project2.com" className="text-custom-green">
+            Project Title 2
+          </a>
+          <p className="italic">Metadata for Project 2</p>
+        </div>
+        <div className="border p-4">
+          <img
+            src="path/to/image3.jpg"
+            alt="Project 3"
+            className="w-full h-auto"
+          />
+          <a href="https://link-to-project3.com" className="text-custom-green">
+            Project Title 3
+          </a>
+          <p className="italic">Metadata for Project 3</p>
+        </div>
+      </div> */}
     </div>
   );
 }
--- src/app/writing/page.tsx
+++ src/app/writing/page.tsx
@@ -2,7 +2,52 @@ export default function Writing() {
   return (
     <div className="py-8">
       {/* <h1 className="text-3xl font-bold mb-4">My Projects</h1> */}
-      <p>Coming soon!</p>
+      <p>
+        I publish personal essays that matter to me at jasminexli.substack.com.{" "}
+      </p>
+      <p>See a hodgepodge of my other favorite essays & thoughts below:</p>
+      <ul className="py-4">
+        <li className="mb-2">
+          <a
+            href="https://docs.google.com/document/d/1p1BU_7JEn5M6hRSnRA9W_WbzVTuEC7bu/edit?usp=drive_link&ouid=113458154265672469537&rtpof=true&sd=true"
+            className="text-custom-green"
+          >
+            Space Odyssey Made Real? Interrogating the Moral Bounds of the
+            AI-Human Friendship through Aristotelian and Confucian Frameworks
+          </a>
+          <p className="italic">
+            Dec 17, 2023 | Cornell James E. Rice, Jr., Prize
+          </p>
+        </li>
+        <li className="mb-2">
+          <a
+            href="https://docs.google.com/document/d/1L0C0s5t7LlKUAxJQ6yJ-c_EUr4OWnj0H9jy0X5BMF3A/edit?usp=sharing"
+            className="text-custom-green"
+          >
+            notes on chinese 'satellite teens'
+          </a>
+          <p className="italic">Jan 11, 2024</p>
+        </li>
+        <li className="mb-2">
+          <a
+            href="https://cornellsun.com/2024/09/10/old-remedies-new-perspectives-finding-healing-in-chinese-medicine/"
+            className="text-custom-green"
+          >
+            Finding healing in Chinese medicine
+          </a>
+          <p className="italic">Sep 10, 2024 | Cornell Daily Sun</p>
+        </li>
+        <li className="mb-2">
+          <a
+            href="https://docs.google.com/document/d/1FZY9LtMMEu3LzgRd2g2E-NwNGsciOwK8/edit?usp=drive_link&ouid=113458154265672469537&rtpof=true&sd=true"
+            className="text-custom-green"
+          >
+            A Cautionary Tale: Internally Conflicting Panoptic Values and
+            Self-Destruction in Black Swan
+          </a>
+          <p className="italic">Sep 10, 2024 | Stanford De Novo</p>
+        </li>
+      </ul>
     </div>
   );
 }
