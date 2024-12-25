7de035b21fd6324dde44b5d5a63308872ec1d5aa
Jasmine Li <jxl9@cornell.edu>
2024-12-24 23:11:06 -0600

    add resume

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -2,7 +2,7 @@ export default function Resume() {
   return (
     <div className="py-8">
       <iframe
-        src="https://drive.google.com/file/d/1m917O1h1YSGYy7eMl-rCvRHSy5VYfCH6/view?usp=sharing"
+        src="https://drive.google.com/file/d/1m917O1h1YSGYy7eMl-rCvRHSy5VYfCH6/preview"
         width="100%"
         height="1000"
         allow="autoplay"
